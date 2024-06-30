8d846e3717e53587faf95b596ef7f8c2d2915098
Bryan Tchakote <tchakomanuel@gmail.com>
2024-06-30 18:42:26 +0200

    Check if document is in search area

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ with st.sidebar:
 	with st.expander("Consulter la base de connaissances"):
 	  # Retrieve the list of saved docs
 		index_configs = RagService.list_vector_store_index()
+
+		# List to check if documents are in search area
+		is_in_search_area = [None] * len(index_configs)
 		
 		for i, index_config in enumerate(index_configs):
 			# Get document name
@@ -37,7 +40,7 @@ with st.sidebar:
 			# Column 1: Add document in search area
 			label = document_name[:8] + "..." + document_extension
 			help = "Inclure ce document dans le domaine de recherche de la réponse"
-			add_in_search_area.toggle(label=label, value=True, help=help)
+			is_in_search_area[i] = add_in_search_area.toggle(label=label, value=True, help=help)
 
 			# Column 2: Summarize document
 			summarize.button(label=":memo:", key=f"summarize_{document_name}", help="Résumer ce document", on_click=None, args=None, use_container_width=True)
