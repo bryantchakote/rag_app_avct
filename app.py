8de3aad93c041fbf57a54577d9f84150a2073b16
Bryan Tchakote <tchakomanuel@gmail.com>
2024-06-30 17:18:57 +0200

    Added triggers for document selection, summarization, and deletion

--- app.py
+++ app.py
@@ -20,4 +20,19 @@ with st.sidebar:
 			document_path = Path(index_config["document_path"])
 			document_name = document_path.name
 			document_extension = document_name.split(".")[-1]
-			st.markdown(document_name[:10] + "..." + document_extension)
+
+			# Display document name, allow selection in user question search area,
+			# and add document summarization and file deletion buttons
+			add_in_search_area, summarize, delete = st.columns([8, 1, 1])
+
+			# Column 1: Add document in search area
+			label = document_name[:10] + "..." + document_extension
+			help = "Inclure ce document dans le domaine de recherche de la réponse"
+			add_in_search_area.toggle(label=label, value=True, help=help)
+
+			# Column 2: Summarize document
+			summarize.button(label=":memo:", key=f"summarize_{document_name}", help="Résumer ce document", on_click=None, args=None, use_container_width=True)
+			
+			# Column 3: Delete document
+			delete.button(label=":x:", key=f"delete_{document_name}", help="Supprimer ce document", on_click=None, args=None, use_container_width=True)
+			
